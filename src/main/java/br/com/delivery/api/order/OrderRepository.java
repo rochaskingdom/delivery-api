1d0572b467ae05c3f69e6faa5d6e61271710edb5
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-01-13 12:03:02 -0300

    Criacao do recurso de pedidos, metodo get

--- src/main/java/br/com/delivery/api/order/OrderRepository.java
+++ src/main/java/br/com/delivery/api/order/OrderRepository.java
@@ -1,10 +1,16 @@
 package br.com.delivery.api.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    @Query("SELECT DISTINCT o FROM Order o JOIN FETCH o.products "
+            + " WHERE o.status = 0 ORDER BY o.moment ASC")
+    List<Order> findOrdersWithProducts();
 }
 
--- src/main/java/br/com/delivery/api/order/OrderService.java
+++ src/main/java/br/com/delivery/api/order/OrderService.java
@@ -1,7 +1,19 @@
 package br.com.delivery.api.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Transactional(readOnly = true)
+    public List<Order> findAll() {
+        return orderRepository.findOrdersWithProducts();
+    }
 }
